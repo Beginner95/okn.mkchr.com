6eb259ff9741c273736e8d32566196d13db2185b
Vaharsolta <vaha-22@mail.ru>
2020-03-15 18:08:12 +0300

    Added method construct auth

--- app/Http/Controllers/ComplexController.php
+++ app/Http/Controllers/ComplexController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class ComplexController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(Request $request)
     {
         $limit = $request['limit'];
