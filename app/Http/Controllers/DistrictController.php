44a8dbb6030c1b290a14ed260fb81d7192f44cd3
Vaharsolta <vaha-22@mail.ru>
2020-04-11 17:37:04 +0300

    Added method get district id

--- app/Http/Controllers/DistrictController.php
+++ app/Http/Controllers/DistrictController.php
@@ -7,6 +7,18 @@ use Illuminate\Http\Request;
 
 class DistrictController extends Controller
 {
+    public static function getDistrictId($districtName)
+    {
+        if (!empty($districtName)) {
+            $district = new District();
+            $district = $district->getFindByName($districtName);
+            $district = !empty($district) ? $district->id : null;
+        } else {
+            $district = null;
+        }
+        return $district;
+    }
+
     public function autocompleteDistrict(Request $request)
     {
         $searchquery = $request->queryString;
