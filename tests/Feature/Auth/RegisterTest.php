6d3bbed82f777afe1ccf22f10a4cd9b621c99e6f
Vaharsolta <vaha-22@mail.ru>
2020-07-30 22:36:21 +0300

    Implemented a test for users registration

---
+++ tests/Feature/Auth/RegisterTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RegisterTest extends TestCase
+{
+    use DatabaseMigrations;
+    public function testForm(): void
+    {
+        $response = $this->get('/register');
+
+        $response
+            ->assertStatus(200)
+            ->assertSee('Register');
+    }
+
+    public function testErrors(): void
+    {
+        $response = $this->post('/register', [
+            'name' => '',
+            'email' => '',
+            'password' => '',
+            'password_confirmation' => '',
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertSessionHasErrors(['name', 'email', 'password']);
+    }
+
+    public function testSuccess(): void
+    {
+        $user = factory(User::class)->make();
+
+        $response = $this->post('/register', [
+            'name' => $user->name,
+            'email' => $user->email,
+            'password' => 'secret',
+            'password_confirmation' => 'secret',
+        ]);
+
+        $response
+            ->assertStatus(302)
+            ->assertRedirect('/')
+            ->assertSessionHas('success', '');
+    }
+}
