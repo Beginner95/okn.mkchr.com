7ed0478d8e7514c73f91625d3aebf7e573012c21
Vaharsolta <vaha-22@mail.ru>
2020-10-12 19:01:50 +0300

    Basic localization file

---
+++ resources/lang/ru/basic.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'choose' => '--Выбрать--',
+    'filter' => 'Отфильтровать',
+    'add' => 'Добавить',
+    'columns' => 'Столбцы',
+    'decrease_font' => 'Уменьшить шрифт',
+    'zoom_font' => 'Увеличить шрифт',
+    'decrease_icon' => 'A-',
+    'zoom_icon' => 'A+',
+    'number_icon' => '№',
+    'latitude' => 'Широта',
+    'longitude' => 'Долгота',
+    'comment' => 'Примечание',
+    'action' => 'Действие',
+    'edit' => 'Редактировать',
+    'delete' => 'Удалить',
+    'save' => 'Сохранить',
+
+    'file' => 'Файл',
+    'add_file' => 'Добавить файл',
+    'add_new_file' => 'Приложить новый файл',
+    'download' => 'Скачать',
+    'delete_file' => 'Удалить файл',
+    'supported_formats' => 'Поддерживаемые форматы файлов jpg, pdf, zip'
+];
