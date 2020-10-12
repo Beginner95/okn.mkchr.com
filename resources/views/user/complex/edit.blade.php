3c0781bfd6bcfa70b389fa38bb0754edc12be9dd
Vaharsolta <vaha-22@mail.ru>
2020-10-12 19:04:17 +0300

    Localized templates for working with complexes

--- resources/views/user/complex/edit.blade.php
+++ resources/views/user/complex/edit.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-12">
                 <div class="card block-weight">
                     <div class="card-header">
-                        Добавление нового комплекса (ансамбля)
+                        {{ trans('complex.edit') }}
                     </div>
                     <div class="card-body">
                         <form action="{{ route('complex.update') }}/{{$okn->id}}" method="post" enctype="multipart/form-data">
@@ -14,24 +14,20 @@
                             <div class="form-group">
                                 <div class="row">
                                     <div class="col">
-                                        <label for="">
-                                            Наименование комплекса (ансамбля)
-                                        </label>
+                                        <label for="">{{ trans('complex.name_ru') }}</label>
                                         <textarea
                                             name="okn-name"
                                             class="form-control"
-                                            title="Наименование объекта культурного наследия (ОКН)"
+                                            title="{{ trans('complex.name_ru') }}"
                                             required>{{ $okn->name }}
                                         </textarea>
                                     </div>
                                     <div class="col">
-                                        <label for="">
-                                            Наименование комплекса (ансамбля) на чеченском языке
-                                        </label>
+                                        <label for="">{{ trans('complex.name_chr') }}</label>
                                         <textarea
                                             name="okn-name-chr"
                                             class="form-control"
-                                            title="Наименование объекта культурного наследия на чеченском языке">{{ $okn->name_chr }}
+                                            title="{{ trans('complex.name_chr') }}">{{ $okn->name_chr }}
                                         </textarea>
                                     </div>
                                 </div>
@@ -40,23 +36,21 @@
                             <div class="form-group">
                                 <div class="row">
                                     <div class="col">
-                                        <label for="origin">
-                                            Датировка ОКН комплекса (ансамбля)
-                                        </label>
+                                        <label for="origin">{{ trans('complex.date') }}</label>
                                         <input
                                             type="text"
                                             name="date-okn"
                                             class="form-control"
-                                            title="Датировка ОКН"
+                                            title="{{ trans('complex.date') }}"
                                             value="{{ $okn->date_okn }}">
                                     </div>
                                     <div class="col">
-                                        <label for="origin">Акт о постановке на гос. учет / о принятии на гос. охрану</label>
+                                        <label for="origin">{{ trans('complex.act') }}</label>
                                         <input
                                             type="text"
                                             name="act"
                                             class="form-control"
-                                            title="Нормативный Акт о постановке на гос. учет // о принятии на гос. охрану"
+                                            title="{{ trans('complex.act') }}"
                                             value="{{ $okn->act }}">
                                     </div>
                                 </div>
@@ -68,17 +62,17 @@
                                         id="districtName"
                                         class="col"
                                         data-district-name="{{ !empty($okn->district->name) ? $okn->district->name : '' }}">
-                                        <label for="">Адрес</label>
+                                        <label for="">{{ trans('complex.address') }}</label>
                                         <autocomplete-district></autocomplete-district>
                                     </div>
                                     <div class="col">
-                                        <label for="">Дополните адрес</label>
+                                        <label for="">{{ trans('complex.additional_address') }}</label>
                                         <input
                                             type="text"
                                             name="address"
                                             class="form-control"
-                                            title="Дополните адрес после района"
-                                            placeholder="Дополните адрес после района"
+                                            title="{{ trans('complex.additional_address') }}"
+                                            placeholder="{{ trans('complex.additional_address') }}"
                                             value="{{ $okn->address }}">
                                     </div>
                                 </div>
@@ -87,17 +81,17 @@
                             <div class="form-group">
                                 <div class="row">
                                     <div class="col">
-                                        <label for="">Категория историко-культурного значения</label>
-                                        <select name="category" id="" class="form-control" title="Категория историко-культурного значения">
-                                            <option value="0" @if($okn->category === '0') selected @endif>Республиканского значения</option>
-                                            <option value="1" @if($okn->category === '1') selected @endif>Федерального значения</option>
+                                        <label for="">{{ trans('complex.category') }}</label>
+                                        <select name="category" id="" class="form-control" title="{{ trans('complex.category') }}">
+                                            <option value="0" @if($okn->category === '0') selected @endif>{{ trans('complex.category_republic') }}</option>
+                                            <option value="1" @if($okn->category === '1') selected @endif>{{ trans('complex.category_federal') }}</option>
                                         </select>
                                     </div>
                                     <div class="col">
-                                        <label for="">Собственность ОКН (ФС/РС)</label>
-                                        <select name="owner" id="" class="form-control" title="Собственность ОКН (ФС/РС)">
-                                            <option value="0" @if($okn->owner === '0') selected @endif>Республиканского значения</option>
-                                            <option value="1" @if($okn->owner === '1') selected @endif>Федерального значения</option>
+                                        <label for="">{{ trans('complex.owner') }}</label>
+                                        <select name="owner" id="" class="form-control" title="{{ trans('complex.owner') }}">
+                                            <option value="0" @if($okn->owner === '0') selected @endif>{{ trans('complex.owner_republic') }}</option>
+                                            <option value="1" @if($okn->owner === '1') selected @endif>{{ trans('complex.owner_federal') }}</option>
                                         </select>
                                     </div>
                                 </div>
@@ -106,39 +100,39 @@
                             <div class="form-group">
                                 <div class="row">
                                     <div class="col">
-                                        <label for="">Широта</label>
+                                        <label for="">{{ trans('basic.latitude') }}</label>
                                         <input
                                             name="latitude"
                                             class="form-control"
-                                            title="Широта"
+                                            title="{{ trans('basic.latitude') }}"
                                             value="{{ $okn->latitude }}">
                                     </div>
                                     <div class="col">
-                                        <label for="">Долгота</label>
+                                        <label for="">{{ trans('basic.longitude') }}</label>
                                         <input
                                             type="text"
                                             name="longitude"
                                             class="form-control"
-                                            title="Долгота"
+                                            title="{{ trans('basic.longitude') }}"
                                             value="{{ $okn->longitude }}">
                                     </div>
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="">Примечание</label>
+                                <label for="">{{ trans('basic.comment') }}</label>
                                 <textarea name="comment" class="form-control">{{ $okn->comment }}</textarea>
                             </div>
                             <div class="form-group">
                             @if (!empty($okn->file))
                                 <input type="hidden" value="{{ $okn->file }}" id="fileName" name="file-name">
-                                <a href="{{ '/files/' . $okn->file }}" class="btn btn-dark" v-if="!isVisibleFile">Скачать файл</a>
+                                <a href="{{ '/files/' . $okn->file }}" class="btn btn-dark" v-if="!isVisibleFile">{{ trans('basic.download') }}</a>
                                 <a
                                     id="deleteFile"
                                     v-if="!isVisibleFile"
                                     href="#"
                                     class="btn btn-danger"
                                     @click="isVisibleFile = !isVisibleFile">
-                                    Удалить файл
+                                    {{ trans('basic.delete_file') }}
                                 </a>
                             @else
                                 <a
@@ -146,19 +140,19 @@
                                     v-if="!isVisibleFile"
                                     @click="isVisibleFile = !isVisibleFile"
                                     class="btn btn-success">
-                                    Добавить файл
+                                    {{ trans('basic.add_file') }}
                                 </a>
                             @endif
                             </div>
                             <div class="form-group" v-show="isVisibleFile">
-                                <label for="">Приложить новый файл</label>
+                                <label for="">{{ trans('basic.add_new_file') }}</label>
                                 <input
                                     type="file"
                                     name="file"
-                                    title="Поддерживаемые форматы файлов jpg, pdf, zip"
+                                    title="{{ trans('basic.supported_formats') }}"
                                     class="custom-file">
                             </div>
-                            <button type="submit" class="btn btn-primary">Сохранить</button>
+                            <button type="submit" class="btn btn-primary">{{ trans('basic.save') }}</button>
                         </form>
                     </div>
                 </div>
